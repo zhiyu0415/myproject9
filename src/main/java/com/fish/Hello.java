5e79e074235276fd5f056a7dcdd493773ceb44da
fish <yvonneli0415@gmail.com>
2022-06-10 16:18:58 +0800

    Hello world with class Person

--- src/main/java/com/fish/Hello.java
+++ src/main/java/com/fish/Hello.java
@@ -2,6 +2,8 @@ package com.fish;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        //System.out.println("Hello world");
+        Person p = new Person();
+        p.greeting();
     }
 }
