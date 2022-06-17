d54bce7e9f1cf71a47959f1868e3eafe3a6d02ad
fish <yvonneli0415@gmail.com>
2022-06-17 14:30:47 +0800

    score

---
+++ src/main/java/com/fish/score/Student.kt
@@ -0,0 +1,12 @@
+package com.fish.com.fish.score
+
+class Student {
+    //val name = "" //傳進來就不能改變了
+    var name = " "
+    var math = 0
+    var english = 0
+}
+
+fun main() {
+    val stu = Student("Jack",88,57)
+}
