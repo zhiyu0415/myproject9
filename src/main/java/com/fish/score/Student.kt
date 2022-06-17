ee64fd25729ca46cf0adbf8aae737f448c9b24fc
fish <yvonneli0415@gmail.com>
2022-06-17 15:17:51 +0800

    kotlin student

--- src/main/java/com/fish/score/Student.kt
+++ src/main/java/com/fish/score/Student.kt
@@ -1,12 +1,27 @@
 package com.fish.com.fish.score
 
-class Student {
+class Student (var name:String,var math:Int,var english:Int){
     //val name = "" //傳進來就不能改變了
-    var name = " "
+    /*var name = " "
     var math = 0
-    var english = 0
+    var english = 0*/
+    //init 執行建構子的內容
+    /*init{
+            println("testing")
+    }
+     */
+    fun print(){
+        println("$name\t$math\t$english\t${getAverage()}") //資料直接拿到 $ 經過手續才拿的到${}
+    }
+    /*fun getAverage():Int{
+        return(math+english)
+    }*/
+    fun getAverage() = (math+english)/2
+
+
 }
 
 fun main() {
     val stu = Student("Jack",88,57)
+    stu.print()
 }
